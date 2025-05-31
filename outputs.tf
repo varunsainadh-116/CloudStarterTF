ccbb6cd8911f102a1a287c92aa418d30382bc5ab
Varun <korumillivarunsainadh@gmail.com>
2025-05-31 17:40:33 +0530

    Done

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,12 @@ output "ec2_instance_id" {
 output "ec2_instance_private_ip" {
   value = aws_instance.ec2_instance.private_ip
 }
+
+output "ec2_instance_ami" {
+  value = aws_instance.ec2_instance.ami
+}
+
+output "ec2_instance_name" {
+  value = aws_instance.ec2_instance.tags["Name"]
+
+}
